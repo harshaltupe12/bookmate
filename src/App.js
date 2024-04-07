c5e57aa5c0581cdce1351b6bb94ec896c1ebfd2a
harshaltupe12 <harshaltupe12@gmail.com>
2024-04-07 16:36:56 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { Route, Routes } from "react-router-dom";
+//css
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+//pages
+import RegisterPage from "./pages/Register";
+import ListingPage from "./pages/List";
+import HomePage from "./pages/Home";
+import BookDetailPage from "./pages/Details";
+import OrdersPage from "./pages/ViewOrders";
+import UserPage from "./pages/User";
+import LoginPage from "./pages/Login";
+//components
+import MyNavbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <MyNavbar/>
+      <Routes>
+        <Route path="/" element={<HomePage/>}></Route>
+        <Route path="/register" element={<RegisterPage />}></Route>
+        <Route path="/book/list" element={<ListingPage />}></Route>
+        <Route path="/book/view/:bookId" element={<BookDetailPage />}></Route>
+        <Route path="/book/orders" element={<OrdersPage />}></Route>
+        <Route path="/user" element={<UserPage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import { useFirebase } from "../context/Firebase";
+import BookCard from "../components/Card";
+import "../style/Home.scss"
+
+
+const HomePage = () => {
+  const firebase = useFirebase();
+
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    firebase.listAllBooks().then((books) => setBooks(books.docs));
+  }, []);
+
+  return (
+    <div className="container main ">
+      <div className="row sub">
+        {books.map((book) => (
+          <div key={book.id} className="col-md-3 mb-3">
+            <BookCard id={book.id} {...book.data()} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+  
+};
+
+export default HomePage;
